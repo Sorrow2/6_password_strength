c3804fdb4b71489cfe0f8d0694ffa7d51c2e793d
kartabayanov <kartabayanov@yandex.ru>
2016-12-01 17:35:57 +0300

    задание 6 reword

--- password_strength.py
+++ password_strength.py
@@ -9,38 +9,59 @@ def load_wordlist_file(filepath):
     return wordlist_set
 
 
+def check_upper_and_lower_case(password):
+    return not password.islower() and not password.isupper()
+
+
+def check_special_characters(password):
+    return re.search('[^a-zа-яё0-9]', password, re.IGNORECASE)
+
+
+def check_digits(password):
+    return re.search('\d', password)
+
+
+def check_russian_and_english_letters(password):
+    return re.search('[a-z]', password, re.IGNORECASE) and re.search('[а-яё]', password, re.IGNORECASE)
+
+
+def check_date_and_plate_number(password):
+    return re.match('\d+(января|февраля|марта|апреля|мая|июня|июля|августа|сентября|октября|ноября|декабря|rus|рус)',
+                    password, re.IGNORECASE)
+
+
 def get_password_strength(password):
-    count = 0
     password_length = len(password)
 
-    if password_length > 3:
-        count += 1
-    if password_length > 6:
-        count += 1
-    if password_length > 10:
-        count += 1
-    if password_length > 18:
-        count += 1
+    if password_length < 6:
+        return 0, 'Длина пароля слишком маленькая.'
 
-    if password not in most_common_passwords:
+    if password in most_common_passwords:
+        return 0, 'Этот пароль один из наиболее часто используемых.'
+
+    count = 1
+    if password_length > 10:
         count += 1
 
     if password not in ru_en_wordlist:
         count += 1
 
-    if not password.islower() and not password.isupper():
+    if not check_date_and_plate_number(password):
         count += 1
 
-    if re.search('\d', password):
+    if check_upper_and_lower_case(password):
         count += 1
 
-    if re.search('[^a-zа-яё0-9]', password, re.IGNORECASE):
+    if check_digits:
         count += 1
 
-    if re.search('[a-z]', password, re.IGNORECASE) and re.search('[а-яё]', password, re.IGNORECASE):
-        count += 1
+    if check_special_characters(password):
+        count += 2
 
-    return count
+    if check_russian_and_english_letters(password):
+        count += 2
+
+    return count, ''
 
 
 if __name__ == '__main__':
@@ -49,9 +70,9 @@ if __name__ == '__main__':
         'english_words.txt') | load_wordlist_file('translit_words.txt')
 
     while 1:
-        password = input('Введите пароль: ')
+        password = input('Введите пароль: ').strip()
         if password in ('quit', 'exit'):
             print('Программа завершена.')
             break
-        score = get_password_strength(password)
-        print('Сложность вашего пароля: {}/10.'.format(score))
+        score, msg = get_password_strength(password)
+        print('Сложность вашего пароля: {}/10. {}'.format(score, msg))
